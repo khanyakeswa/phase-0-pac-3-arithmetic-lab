ba351a1a3f26dc5b2e988c1c1c5a1fd52fbf51ae
Khanya Keswa <khanya.keswa@gmail.com>
2022-09-29 22:19:06 -0500

    assignment complete

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+function makeInt(n) {
+    let result = parseInt(n, 10);
+    return result;
+}
+
+function preserveDecimal(n) {
+    let result = parseFloat(n);
+    return result;
+}
